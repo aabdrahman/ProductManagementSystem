0da12345f3db8cf64bca8d189f6d6ffd51d16a61
Abdrahman Akande <Abdrahman.Akande@parallexbank.com>
2026-02-07 11:31:14 +0100

    ConfigureSwaggerAndLogger

--- src/ProductManagementSystem.Api/Program.cs
+++ src/ProductManagementSystem.Api/Program.cs
@@ -1,10 +1,34 @@
+
+
+using Serilog;
+using System.Text.Json;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 
+string logPath = Path.Combine(AppDomain.CurrentDomain.BaseDirectory, "Logs", "log-.txt");
+
+Log.Logger = new LoggerConfiguration()
+                    .MinimumLevel.Information()
+                    .Enrich.FromLogContext()
+                    .WriteTo.File(logPath, Serilog.Events.LogEventLevel.Information, outputTemplate: "[{Timestamp:yyyy-MM-dd HH:mm:ss.ffff zzz}||{Level:u3}] || [{ClassName}].[{MethodName}] - {Message:lj}{NewLine}{Exception}{NewLine}", 
+                                    fileSizeLimitBytes: 10_000_000, rollingInterval: RollingInterval.Day, buffered: false)
+                    .CreateLogger();
+
+builder.Services.AddSwaggerGen();
+builder.Services.AddControllers();
+
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
+
+app.UseSwagger();
+app.UseSwaggerUI(opts =>
+{
+    opts.RoutePrefix = string.Empty;
+    opts.SwaggerEndpoint("/swagger/v1/swagger.json", "Product Management System");
+});
 
 app.UseHttpsRedirection();
 
@@ -23,8 +47,19 @@ app.MapGet("/weatherforecast", () =>
             summaries[Random.Shared.Next(summaries.Length)]
         ))
         .ToArray();
+    Log
+    .ForContext("MethodName", "GetForecasts")
+    .ForContext("ClassName", "Program")
+    .Information($"{JsonSerializer.Serialize(forecast)}");
     return forecast;
 });
+
+app.MapGet("/", () =>
+{
+    return $"Hello World!!!From {Environment.OSVersion.Version}";
+});
+
+app.MapControllers();
 
 app.Run();
 
