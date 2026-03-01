f7e37fef7f38b572064c2397e8ce48736accb659
Akande Abdrahman <akandeabdrahman@gmail.com>
2026-03-01 12:17:36 +0100

    ConfigureProductCategoryOperation_Add/Read-FrontEnd

---
+++ src/ProductManagementSystem.Client/Handlers/AddProductCategoryHandler.cs
@@ -0,0 +1,34 @@
+﻿using ProductManagementSystem.Shared.DataTransferObjects.ProductCategory;
+using ProductManagementSystem.Shared.DataTransferObjects.Response;
+using System.Text.Json;
+
+namespace ProductManagementSystem.Client.Handlers;
+
+public class AddProductCategoryHandler
+{
+    private readonly HttpClient _httpClient;
+
+    public AddProductCategoryHandler(IHttpClientFactory httpClientFactory, IConfiguration configuration)
+    {
+        _httpClient = httpClientFactory.CreateClient(configuration.GetValue<string>("ApiClient:Key") ?? throw new ArgumentNullException("The api key name is not provided yet"));
+    }
+
+    public async Task<(bool isSuccessful, string responseMessage)> Handle(string productCategoryToCreate)
+    {
+        try
+        {
+            var httpResponse = await _httpClient.PostAsJsonAsync("api/productcategory", productCategoryToCreate);
+
+            string responseContent = await httpResponse.Content.ReadAsStringAsync();
+
+            GenericResponse<ProductCategoryDto> responseBody = JsonSerializer.Deserialize<GenericResponse<ProductCategoryDto>>(responseContent, new JsonSerializerOptions() { PropertyNameCaseInsensitive = true }) ??
+                                                                    throw new ArgumentNullException("Response could not be deserialized.");
+
+            return (responseBody.IsSuccessStatus, responseBody.ResponseMessage);
+        }
+        catch (Exception ex)
+        {
+            return (false, ex.Message);
+        }
+    }
+}
--- src/ProductManagementSystem.Client/Program.cs
+++ src/ProductManagementSystem.Client/Program.cs
@@ -16,6 +16,7 @@ builder.Services.AddScoped<RestockProductHandler>();
 builder.Services.AddScoped<GetProductUpdateDetailsHandler>();
 builder.Services.AddScoped<UpdateProductHandler>();
 builder.Services.AddScoped<DeleteProductHandler>();
+builder.Services.AddScoped<AddProductCategoryHandler>();
 
 builder.Services.AddHttpClient(builder.Configuration.GetValue<string>("ApiClient:Key") ?? throw new ArgumentNullException("The api key name is not provided yet"), opts =>
 {
