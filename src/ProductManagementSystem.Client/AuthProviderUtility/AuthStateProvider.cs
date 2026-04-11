fb2f5342319868a8a648a95b8ebc62fa51ce8b1b
Abdrahman Akande <akandeabdrahman@gmail.com>
2026-04-11 16:53:47 +0100

    Modify_Admin_Layout_Authentication_Add_Rfresh_Implementation_In_Auth_Provider

--- src/ProductManagementSystem.Client/AuthProviderUtility/AuthStateProvider.cs
+++ src/ProductManagementSystem.Client/AuthProviderUtility/AuthStateProvider.cs
@@ -1,4 +1,5 @@
 ﻿using Microsoft.AspNetCore.Components.Authorization;
+using ProductManagementSystem.Client.Handlers;
 using ProductManagementSystem.Client.Utilities;
 using ProductManagementSystem.Client.Utilities.Contracts;
 using ProductManagementSystem.Shared.DataTransferObjects.Authentication;
@@ -9,14 +10,16 @@ namespace ProductManagementSystem.Client.AuthProviderUtility;
 public class AuthStateProvider : AuthenticationStateProvider
 {
     private readonly ILocalStorageUtility _storageUtility;
+    private readonly RefreshTokenHandler _refreshTokenHandler;
     private AuthenticationState _anonymous;
     private readonly TokenContainer _tokenContainer;
 
-    public AuthStateProvider(ILocalStorageUtility storageUtility, TokenContainer tokenContainer)
+    public AuthStateProvider(ILocalStorageUtility storageUtility, TokenContainer tokenContainer, RefreshTokenHandler refreshTokenHandler)
     {
         _storageUtility = storageUtility;
         _anonymous = new AuthenticationState(new ClaimsPrincipal(new ClaimsIdentity()));
         _tokenContainer = tokenContainer;
+        _refreshTokenHandler = refreshTokenHandler;
     }
 
     public override async Task<AuthenticationState> GetAuthenticationStateAsync()
@@ -48,13 +51,56 @@ public class AuthStateProvider : AuthenticationStateProvider
         }
 
         DateTimeOffset expiryTimestamp = DateTimeOffset.FromUnixTimeSeconds(expiryTime);
-        //Console.WriteLine("Expiry Timestamp - {0}", expiryTimestamp);
 
-        if(DateTimeOffset.UtcNow > expiryTimestamp)
+        if(DateTimeOffset.UtcNow > expiryTimestamp || (DateTimeOffset.UtcNow - expiryTimestamp).TotalSeconds <= 5)
         {
-            var removeTokenResult = await _storageUtility.RemoveItemFromStorageAsync("session-token");
-            NotifyAuthenticationStateChanged(Task.FromResult(_anonymous));
-            return _anonymous;
+            //Begin Refresh Token implementation
+            if(DateTime.UtcNow > storedToken.TokenExpirationTime || (DateTimeOffset.UtcNow - expiryTimestamp).TotalSeconds <= 5)
+            {
+                var refreshTokenResult = await _refreshTokenHandler.Handle(storedToken);
+
+                if (refreshTokenResult)
+                {
+                    storedToken = await _storageUtility.GetItemFromStorageAsync<TokenDto>("session-token");
+
+                    if (storedToken is null)
+                    {
+                        NotifyAuthenticationStateChanged(Task.FromResult(_anonymous));
+                        return _anonymous;
+                    }
+
+                    claimsPrincipal = JwtParser.ParseClaimsFromJwt(storedToken.Token);
+
+                    if (DateTime.UtcNow >= storedToken.TokenExpirationTime)
+                    {
+                        var removeTokenResult = await _storageUtility.RemoveItemFromStorageAsync("session-token");
+                        NotifyAuthenticationStateChanged(Task.FromResult(_anonymous));
+                        return _anonymous;
+                    }
+
+                    expTokenTime = claimsPrincipal.FirstOrDefault(x => x.Type.Contains("exp", StringComparison.CurrentCultureIgnoreCase))?.Value ?? "";
+
+                    if (!long.TryParse(expTokenTime, out expiryTime))
+                    {
+                        var removeTokenResult = await _storageUtility.RemoveItemFromStorageAsync("session-token");
+                        NotifyAuthenticationStateChanged(Task.FromResult(_anonymous));
+                        return _anonymous;
+                    }
+                }
+                else
+                {
+                    var removeTokenResult = await _storageUtility.RemoveItemFromStorageAsync("session-token");
+                    NotifyAuthenticationStateChanged(Task.FromResult(_anonymous));
+                    return _anonymous;
+                }
+            }
+            else
+            {
+                var removeTokenResult = await _storageUtility.RemoveItemFromStorageAsync("session-token");
+                NotifyAuthenticationStateChanged(Task.FromResult(_anonymous));
+                return _anonymous;
+            }
+
         }
 
         _tokenContainer.SetToken(storedToken.Token);
