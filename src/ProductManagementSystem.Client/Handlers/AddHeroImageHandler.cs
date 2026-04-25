d82e39e49476a02946a067ea86a759325645bea2
Abdrahman Akande <akandeabdrahman@gmail.com>
2026-04-25 11:36:04 +0100

    Fix_Image_Operation-Client

--- src/ProductManagementSystem.Client/Handlers/AddHeroImageHandler.cs
+++ src/ProductManagementSystem.Client/Handlers/AddHeroImageHandler.cs
@@ -15,25 +15,39 @@ public class AddHeroImageHandler
     {
 		try
 		{
+            Console.WriteLine("Begin Handler operation...");
             var filePath = Path.Combine(_env.WebRootPath, "Images", uploadedFile.Name);
+            Console.WriteLine($"File path: {filePath}");
 
             //var filePath = Path.Combine(Directory.GetCurrentDirectory(), "wwwroot", "Images");
 
+            using var ms = new MemoryStream();
             using (var fileStream = new FileStream(filePath, FileMode.Create, FileAccess.ReadWrite))
             {
-                var buffer = new byte[uploadedFile.Size];
+                Console.WriteLine("Begin stream operation...");
+                var buffer = new byte[1024 * 16];
+                int bytesRead;
 
-                await uploadedFile.OpenReadStream(uploadedFile.Size).ReadAsync(buffer);
+                while ((bytesRead = await fileStream.ReadAsync(buffer, 0, buffer.Length)) > 0)
+                {
+                    // Write the chunk into our MemoryStream
+                    await ms.WriteAsync(buffer.AsMemory(0, bytesRead));
+                }
 
-                await fileStream.WriteAsync(buffer);
+                await fileStream.CopyToAsync(ms);
+
+                Console.WriteLine("File copied...");
+
+                //await fileStream.WriteAsync(buffer);
 
                 await fileStream.DisposeAsync();
             }
-
+            Console.WriteLine("File stream disposed...");
             return (true, "File Uploaded Successfully.");
         }
 		catch (Exception ex)
 		{
+            Console.WriteLine($"Exception occurred: {ex.Message}");
             return (false, ex.Message);
 		}
     }
